a17ef8df73c9bae4f70e8d2c9dbc928cac6f9d3d
Hugh <huge.mcnamara.dev@gmail.com>
2021-07-16 07:57:10 -0400

    updated firebase config with correct credentials

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,14 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+var config = {
+  apiKey: "AIzaSyCxzbOSXbUeqndW44KFiRqxhzkAuJLQFM8",
+  authDomain: "huge-ecommerce.firebaseapp.com",
+  projectId: "huge-ecommerce",
+  storageBucket: "huge-ecommerce.appspot.com",
+  messagingSenderId: "368029403612",
+  appId: "1:368029403612:web:446a8efab6dbba7333e719"
 };
 
 firebase.initializeApp(config);
